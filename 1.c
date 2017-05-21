119dfd6b54b0de7a76268baf626b025beceab3a5
dbsxotjr100 <dbsxotjr100@gmail.com>
2017-05-22 01:41:53 +0900

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+	int num=0;
+	int i=0, j=0, k=0;
+	printf("줄의 수를 입력하세요 : ");
+	scanf("%d", &num);
+	for(i=0;i<=num;i++){
+		for(j=0;j<num-i;j++){
+			printf(" ");
+		}
+		for(j=0;j<2*i-1;j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	for(i=0;i<num;i++){
+		for(j=0;j<=i;j++){
+			printf(" ");
+		}
+		for(j=0;j<2*(num-i-1)-1;j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+			return 0;
+}
